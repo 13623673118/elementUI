97d4e43e01c9b1e8567859515c45eb789cac5982
zhaochenmin <zhaochenmin@dpandora.cn>
2017-09-15 17:52:44 +0800

    axios

--- src/components/template/date.vue
+++ src/components/template/date.vue
@@ -1,17 +1,28 @@
 <template>
 	<div class="date">
-    <el-button type="danger">默认按钮</el-button>
+    <el-button type="danger" @click="getMsg">默认按钮</el-button>
 	</div>
 </template>
 
 <script>
+  import axios from 'axios'
 	export default {
 	  name:'',
     data(){
 	    return{
 
       }
+    },
+    methods:{
+      getMsg(){
+        axios.get('/').then(function (res) {
+          console.log(res)
+        }).catch(function (err) {
+          console.log(err)
+        })
+      }
     }
+
 	}
 </script>
 <style lang="scss" scoped>
